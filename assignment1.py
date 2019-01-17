a7ee013b1242a32c70ec2a9c765d9ec517cf1549
Joshua Schlichting <42978321+jrs322@users.noreply.github.com>
2019-01-17 14:31:43 -0500

    Update assignment1.py

--- assignment1.py
+++ assignment1.py
@@ -10,15 +10,60 @@ import time
 #green is pin 19
 #blue is pin 26
 def loop():
-    DELAY =
+    DELAY = .01
     pi1 = pigpio.pi()
     pi1.set_PWM_range(9, 100)
     #turn all off
-    pi1.write(13,0)
     pi1.write(19,0)
+    pi1.write(13,0)
     pi1.write(26,0)
     time.sleep(1)
     #begin ramping R value
+    pi1.write(19,1)
+    pi1.set_PWM_range(19, 100)
     for i in range(9,100):
-        pi1.set_PWM_dutycycle(i)
+        pi1.set_PWM_dutycycle(19,i)
         time.sleep(DELAY)
+    for j in range(100, 9, -1):
+        pi1.set_PWM_dutycycle(19 , i)
+        time.sleep(DELAY)
+    pi1.write(19,0)
+    time.sleep(1)
+    #begin ramping up the green led
+    pi1.write(13,1)
+    pi1.set_PWM_range(13, 100)
+    for i in range(9,100):
+        pi1.set_PWM_dutycycle(13,i)
+        time.sleep(DELAY)
+    for j in range(100, 9, -1):
+        pi1.set_PWM_dutycycle(13 , i)
+        time.sleep(DELAY)
+    pi1.write(13,0)
+    time.sleep(1)
+    #begin ramping up the blue led
+    pi1.write(26,1)
+    pi1.set_PWM_range(26, 100)
+    for i in range(9,100):
+        pi1.set_PWM_dutycycle(26,i)
+        time.sleep(DELAY)
+    for j in range(100, 9, -1):
+        pi1.set_PWM_dutycycle(26 , i)
+        time.sleep(DELAY)
+    pi1.write(26,0)
+    time.sleep(1)
+    #begin ramping up the white led which is a mix of all
+    pi1.write(26,1)
+    pi1.write(19,1)
+    pi1.write(13,1)    
+    for i in range(9,100):
+        pi1.set_PWM_dutycycle(26,i)
+        pi1.set_PWM_dutycycle(19,i)
+        pi1.set_PWM_dutycycle(13,i)        
+        time.sleep(DELAY)
+    for j in range(100, 9, -1):
+        pi1.set_PWM_dutycycle(26,i)
+        pi1.set_PWM_dutycycle(19,i)
+        pi1.set_PWM_dutycycle(13,i)
+        time.sleep(DELAY)
+    pi1.write(26,0)
+    time.sleep(10)    
